87acadfa6f5b8f80e6469905f45386c22109dcfe
Gaston Freire <gastonfreire@outlook.com>
2023-02-11 14:34:17 -0300

    video de fondo

--- spaceman/src/App.js
+++ spaceman/src/App.js
@@ -1,10 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import fondoindex2 from '../src/video/fondoindex2.mp4'
 
 function App() {
   return (
     <div className="App">
+
+<video autoPlay loop
+style={{
+position: "fixed",
+minWidth: "100%",
+minHeight: "100%",
+top: "50%",
+left:"50%",}}>
+
+</video>
+
+
+
       <header className="App-header">
+      
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
