fd8ee5fd69e58887c9340c8946dc6fd10ba99b1d
Gaston Freire <gastonfreire@outlook.com>
2023-02-13 22:44:44 -0300

    a

--- spaceman/src/App.js
+++ spaceman/src/App.js
@@ -1,23 +1,39 @@
-import logo from "./logo.svg";
-import "./App.css";
-import fondo1 from "./fondo1.mp4";
+import logo from './logo.svg';
+import './App.css';
+import fondoindex1 from './'
 
 function App() {
   return (
     <div className="App">
+
+<video autoPlay loop src={fondoindex1}
+style={{
+  objectFit: 'cover',
+    width: '100%',
+    height: '100%',
+    position: 'absolute',
+    zIndex: '-1'
+}}>
+
+</video>
+
+
+
       <header className="App-header">
-        <video
-          autoPlay
-          loop
-          src={fondo1}
-          style={{
-            objectFit: "cover",
-            minWidth: "100%",
-            minHeight: "100%",
-            position: "absolute",
-            zIndex: "-1",
-          }}
-        ></video>
+      
+
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
