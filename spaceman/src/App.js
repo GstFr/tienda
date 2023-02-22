0b856a870bf588fc9d59ee6d27803b0547c44188
Gaston Freire <gastonfreire@outlook.com>
2023-02-21 23:53:23 -0300

    primera entrega lista

--- spaceman/src/App.js
+++ spaceman/src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavBar from "../src/componentes/navbar/navbar";
-import Fondoindex1 from "./3.jpg"
+import Fondoindex1 from "./fondo1.mp4"
 import Boton from "../src/componentes/boton/boton";
 import  ItemListContainer from "./componentes/ItemListContainer/lista";
 
@@ -14,7 +14,7 @@ function App() {
 
       <header className="App-header">
      
-        <img className="video"  src={Fondoindex1} />
+        <video className="video" autoplay muted loop src={Fondoindex1} />
         <Boton />
         <ItemListContainer greeting={"lista"} />
       </header>
