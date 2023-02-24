5ef37bcbea626c90ea667736de37b8c75f739430
Gaston Freire <gastonfreire@outlook.com>
2023-02-24 17:21:07 -0300

    responsive

--- spaceman/src/componentes/ItemListContainer/ItemList/ItemList.js
+++ spaceman/src/componentes/ItemListContainer/ItemList/ItemList.js
@@ -1,31 +1,30 @@
-import "bootstrap/dist/css/bootstrap.min.css";
+
 import React from "react";
 import Item from "./Item/Item";
 import "./ItemList.css"
-import Stack from 'react-bootstrap/Stack';
+
 
 const productos = [
     {
         id:0,
         nombre: "manzana" ,
         precio: 20,
-        imagen :"./product1.jpeg",
+        imagen :"../../public/imagenesprod/product1.jpeg",
     },
     {
         id: 1,
         nombre: "pera" ,
         precio: 30,
-        imagen :"./producto2.jpeg",
+        imagen :"../../public/imagenesprod/producto2.jpeg",
     },
     {
         id:2,
         nombre: "naranja" ,
         precio: 50,
-        imagen :"./producto3.jpeg",
-    },
+        imagen :"../../public/imagenesprod/producto3.jpeg",
 
+}
 ]
-
 export const getFetch = new Promise ((respuesta, rejected) => {
 
 let url = "www.laverdu.com.ar"
